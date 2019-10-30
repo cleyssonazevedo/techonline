0c82544f6dd81ad2af3ef979fb276dcfca630bec
Cleysson Azevedo <cleyssonazevedo@outlook.com>
2019-10-29 22:33:27 -0300

    Icones e design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-home',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+    constructor(
+        private readonly title: Title
+    ) {
+        this.title.setTitle('TechOnline - Home');
+    }
 }
 
--- src/app/templates/default-template/default-template.module.ts
+++ src/app/templates/default-template/default-template.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MenuModule } from 'src/app/components/menu/menu.module';
 import { DefaultTemplateComponent } from './default-template.component';
+import { MenuModule } from 'src/app/components/menu/menu.module';
+import { FooterModule } from 'src/app/components/footer/footer.module';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule,
-        MenuModule
+
+        MenuModule,
+        FooterModule
     ],
     declarations: [ DefaultTemplateComponent ],
     exports: [ DefaultTemplateComponent ]
